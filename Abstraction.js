6d446e9cd4265ea651f7d0a34672bad5c523e6e2
HabibEniolabi <habibullahayomide009@gmail.com>
2026-06-27 07:45:36 +0100

    Getters & Setters

--- Abstraction.js
+++ Abstraction.js
@@ -1,13 +1,28 @@
 function Circle(radius) {
-  this.radius = radius;  
-  let defaultLocation = {x: 0, y: 2}
-  let computeOptimalLocations= function(factor) {
-    //....
-  }
-  this.draw = function () { 
-    computeOptimalLocations(0.1)
+  this.radius = radius;
+  let defaultLocation = { x: 0, y: 2 };
+  this.getDefaultLocation = function () {
+    return defaultLocation;
+  };
+  this.draw = function () {
+    computeOptimalLocations(0.1);
     console.log("draw");
   };
+
+  Object.defineProperty(this, "defaultLocation", {
+    get: function () {
+      return defaultLocation;
+    },
+    set: function(value) {
+      if (!value.x || !value.y)
+        throw new Error("Invalid location.")
+      return defaultLocation
+    }
+  });
 }
 
 const circle = new Circle(1);
+circle.draw();
+circle.defaultLocation = 1;
+
+// Note we use the Object.define properties to define getters or setters
