3c8f9288987fa39136b7bcd26a86ca32e5b5d543
HabibEniolabi <habibullahayomide009@gmail.com>
2026-06-27 08:12:19 +0100

    StopWatch Excercise

---
+++ Excercise.js
@@ -0,0 +1,34 @@
+function Stopwatch() {
+  let startTime,
+    stopTime,
+    running,
+    duration = 0;
+  this.start = function () {
+    if (running) throw new Error("Stopwatch has already started");
+    running = true;
+    startTime = new Date();
+  };
+  this.stop = function () {
+    if (!running) throw new Error("Stopwatch has already stopped");
+    running = false;
+    stopTime = new Date();
+
+    const seconds = (stopTime.getTime() - startTime.getTime()) / 1000;
+    duration += seconds;
+  };
+  this.reset = function () {
+    startTime = null;
+    stopTime = null;
+    reset = false;
+    duration = 0
+  };
+
+  Object.defineProperty(this, duration, {
+    get: function () {
+      return duration;
+    },
+  });
+}
+
+const sw = new Stopwatch();
+sw.reset();
